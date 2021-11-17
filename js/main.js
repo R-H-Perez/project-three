cece31acb5e7d9f2872f8fb779f72a1b0b348081
R-H-Perez <R-H-Perez@users.noreply.github.com>
2021-11-17 08:23:20 +0000

    deploy: 679ab1a4e53d69382a4c47067956d9da1b9f1b1c

--- js/main.js
+++ js/main.js
@@ -1,1 +1,41 @@
+var sectionIndex = 1;
 
+var sections = document.getElementsByClassName("Section");
+var nav = document.getElementsByClassName("Nav");
+
+showSections(sectionIndex);
+
+function changeSections(i) {
+  showSections((sectionIndex += i));
+}
+
+function currentSection(i) {
+  showSections((sectionIndex = i));
+}
+
+function showSections(i) {
+  if (i > sections.length) {
+    sectionIndex = 1;
+  }
+
+  if (i < 1) {
+    sectionIndex = sections.length;
+  }
+
+  Array.from(sections).forEach((item) => (item.style.display = "none"));
+
+  Array.from(nav).forEach(
+    (item) => (item.className = item.className.replace(" selected", ""))
+  );
+
+  sections[sectionIndex - 1].style.display = "block";
+  nav[sectionIndex - 1].className += " selected";
+}
+
+document.querySelector(".LeftButton").addEventListener("click", () => {
+  changeSections(-1);
+});
+
+document.querySelector(".RightButton").addEventListener("click", () => {
+  changeSections(1);
+});
