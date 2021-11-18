75880ec208fc63e6dfd4802f7bff06d2b1c5de8a
Richard Perez <rich2000@email.unc.edu>
2021-11-18 17:01:58 -0500

    Wrote some comments for the JS slideshow.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
+// The max amount of slides start at 1.
 var sectionIndex = 1;
 
+// Each div that has the class name "Section" is set to one slide in the slideshow.
 var sections = document.getElementsByClassName("Section");
+
+// Each span that has the class name "Nav" is set to one dot in the navbar, that is changed by slide changes.
 var nav = document.getElementsByClassName("Nav");
 
 showSections(sectionIndex);
@@ -14,10 +18,12 @@ function currentSection(i) {
 }
 
 function showSections(i) {
+  // If the slide number is set past the max slide (aka if one continues past the last slide that does not exist), the page goes to the first slide in the order (the first slide).
   if (i > sections.length) {
     sectionIndex = 1;
   }
 
+  // If the slide number is set to under 1 (aka if one goes back to a previous slide that does not exist), the page goes to the last slide in the order (the final slide).
   if (i < 1) {
     sectionIndex = sections.length;
   }
@@ -32,14 +38,17 @@ function showSections(i) {
   nav[sectionIndex - 1].className += " selected";
 }
 
+// This wiring is used to make the left button go back to the previous slide.
 document.querySelector(".LeftButton").addEventListener("click", () => {
   changeSections(-1);
 });
 
+// This wiring is used to make the right button continue to the next slide.
 document.querySelector(".RightButton").addEventListener("click", () => {
   changeSections(1);
 });
 
+// Creating the Modal
 // Get
 // button
 let button = document.querySelector("button");
